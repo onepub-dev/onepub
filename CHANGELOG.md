c9ede01b2e4671b834783cd351a25b1d840b2a22
Brett Sutton <bsutton@noojee.com.au>
2021-12-22 15:43:16 +1100

    Released 1.1.0.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 1.1.0
+Asper KayZGames recommendation we now check for a flutter dependency in the pubspec.yaml
+to determine if we should run flutter pub or dart pub.
+
 # 1.0.2
 Better description.
 
--- bin/pub.dart
+++ bin/pub.dart
@@ -1,9 +1,18 @@
 #! /usr/bin/env dcli
 
+import 'dart:io';
+
 import 'package:dcli/dcli.dart';
 
 void main(List<String> arguments) {
-  if (which('flutter').found) {
+  final pubspec = DartProject.fromPath('.').pubSpec;
+  final isFlutter = pubspec.dependencies.containsKey('flutter');
+
+  if (isFlutter) {
+    if (which('flutter').notfound) {
+      printerr(red('Add flutter to your PATH and try again.'));
+      exit(1);
+    }
     startFromArgs(
       'flutter',
       ['pub', ...arguments],
@@ -11,6 +20,10 @@ void main(List<String> arguments) {
       progress: Progress.print(),
     );
   } else {
+    if (which('dart').notfound) {
+      printerr(red('Add dart to your PATH and try again.'));
+      exit(1);
+    }
     DartSdk().runPub(args: arguments, nothrow: true);
   }
 }
