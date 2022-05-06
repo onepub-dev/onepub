12e8d0bc154b3282bc646260387f144a17ac740d
Robert Sutton <rlsutton152@gmail.com>
2022-05-06 17:06:15 +1000

    fix add dependency

--- lib/src/commands/add_dependency.dart
+++ lib/src/commands/add_dependency.dart
@@ -2,7 +2,10 @@ import 'dart:io';
 
 import 'package:args/command_runner.dart';
 import 'package:dcli/dcli.dart';
+import 'package:pub_semver/pub_semver.dart';
+import 'package:pubspec/pubspec.dart';
 
+import '../exceptions.dart';
 import '../onepub_paths.dart';
 import '../onepub_settings.dart';
 
@@ -36,14 +39,37 @@ or updates an existing dependency to pull from your OnePub private repository.
     if (!exists(OnePubPaths().pathToSettingsDir)) {
       createDir(OnePubPaths().pathToSettingsDir, recursive: true);
     }
+    if (argResults!.rest.length != 2) {
+      throw ExitException(exitCode: -1, message: red('''
+The add dependency command takes two arguments:
+onepub add <Package> <Version Constraint>
+'''));
+    }
 
-    final pubspec = project.pubSpec.pubspec
-        .copy(publishTo: Uri.parse(OnePubSettings().onepubWebUrl));
+    final package = argResults!.rest[0];
+    final version = argResults!.rest[1];
+    final organisation = OnePubSettings().obfuscatedOrganisationId;
+    final apiUrl = OnePubSettings().onepubApiUrl;
+    final url = '$apiUrl/$organisation';
+    final versionConstraint = VersionConstraint.parse(version);
+
+    final deps = project.pubSpec.dependencies;
+    final ref = ExternalHostedReference(package, url, versionConstraint, false);
+
+    final dep = Dependency(package, ref);
+
+    final newDependencies = <String, DependencyReference>{};
+    for (final entry in deps.entries) {
+      newDependencies[entry.key] = entry.value.reference;
+    }
+    newDependencies[package] = dep.reference;
+
+    final pubspec = project.pubSpec.pubspec.copy(dependencies: newDependencies);
     await pubspec.save(Directory(project.pathToProjectRoot));
 
     print('''
-${pubspec.name} has been marked as a private package.
-Run 'dart/flutter pub publish' to publish ${pubspec.name} to OnePub
+$package $version has been marked as a private package.
+
 
 See https://onepub.dev/publish
 ''');
