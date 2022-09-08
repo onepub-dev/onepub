df8d08ae84bfeaa5bb0e774ac75d59c3b0ea8409
Brett Sutton <bsutton@noojee.com.au>
2022-09-08 11:17:38 +1000

    Added unit test for missing sub command.

---
+++ test/src/commands/pub_test.dart
@@ -0,0 +1,17 @@
+import 'package:dcli/dcli.dart' hide equals;
+import 'package:onepub/src/entry_point.dart';
+import 'package:onepub/src/my_runner.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('pub - missing sub command', () async {
+    final progress = await DCliZone().run(
+        () async => entrypoint(['pub'], CommandSet.onepub, 'onepub'),
+        progress: Progress.capture());
+
+    final firstline = Ansi.strip(progress.lines.first);
+    expect(firstline, equals('Missing subcommand for "onepub pub".'));
+
+    expect(progress.lines, contains('Available subcommands:'));
+  });
+}
