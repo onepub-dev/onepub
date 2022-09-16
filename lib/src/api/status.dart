4f1d88209f7404e61a2ba6f69bc62bf2feae6f66
Brett Sutton <bsutton@noojee.com.au>
2022-09-17 09:15:51 +1000

    Added server version no. to the status response.

--- lib/src/api/status.dart
+++ lib/src/api/status.dart
@@ -1,6 +1,23 @@
+import 'package:pub_semver/pub_semver.dart';
+
+import '../exceptions.dart';
+
 class Status {
-  Status(this.statusCode, this.message);
+  Status(this.statusCode, this.message, String? version) {
+    if (version == null) {
+      this.version = Version.parse('1.0.0');
+    } else {
+      try {
+        this.version = Version.parse(version);
+      } on FormatException catch (e, _) {
+        throw ExitException(
+            exitCode: -1,
+            message: 'Invalid version "$version" returned by Status command.');
+      }
+    }
+  }
 
   int statusCode;
   String message;
+  late final Version version;
 }
