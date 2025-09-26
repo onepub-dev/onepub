a6228e266046b6d5677d0e81340f688e5150f8ae
Brett Sutton <bsutton@noojee.com.au>
2025-09-26 13:11:53 +1000

    improvements to the onepub pub private command with respect to the messages we output. Better handling of the 'none' publish_to value and fixed the url to the publish doco.

--- lib/src/commands/pub/private.dart
+++ lib/src/commands/pub/private.dart
@@ -7,7 +7,7 @@ import 'package:args/command_runner.dart';
 import 'package:dcli_core/dcli_core.dart';
 import 'package:dcli_input/dcli_input.dart';
 import 'package:dcli_terminal/dcli_terminal.dart';
-import 'package:path/path.dart';
+import 'package:path/path.dart' as p;
 import 'package:pubspec_manager/pubspec_manager.dart';
 import 'package:scope/scope.dart';
 
@@ -64,42 +64,107 @@ run: onepub login
     //   createDir(OnePubPaths.use.pathToSettingsDir, recursive: true);
     // }
     final settings = OnePubSettings.use();
-    final obfuscatedOrganisationId = settings.obfuscatedOrganisationId;
     final currentOrganisationName = settings.organisationName;
     final url = settings.onepubApiUrlAsString;
 
     final pubspec = PubSpec.load(directory: pathToProject);
-    if (pubspec.publishTo.value == url) {
-      print(orange(
-          '${pubspec.name.value} is already a private package of $currentOrganisationName.'));
-      return;
-    }
 
     await API().checkVersion();
-    final organisation = await getOrganisation(obfuscatedOrganisationId);
-    if (organisation == null) {
-      print(orange('${pubspec.name.value} is already a private package '
-          'for another organisation'));
-    } else {
-      print(orange('${pubspec.name.value} is already a private package of '
-          '${organisation.name}'));
-    }
-    if (!await confirm('Do you want to change the organisation to '
-        '$currentOrganisationName?')) {
-      print(red('Action cancelled'));
-      return;
-    }
 
+  /// we need to deal with a number of scenarios
+  /// 1. publish_to is missing
+  /// 2. publish_to exists but is empty.
+  /// 3. publish_to is set to none
+  /// 4. publish_to is set to another org
+  /// 5. publish_to is already set to this org
+    var publish = await _checkForNone(
+        pubspec: pubspec, currentOrganisationName: currentOrganisationName);
+    publish = publish &&
+        await _checkForSelf(
+            pubspec: pubspec,
+            currentOrganisationName: currentOrganisationName,
+            newOrgUrl: url);
+    publish = publish &&
+        await _checkForOther(
+            pubspec: pubspec,
+            currentOrganisationName: currentOrganisationName,
+            newOrgUrl: url);
+
+    if (publish) {
+      _publish(
+          pubspec: pubspec,
+          url: url,
+          pathToProject: pathToProject,
+          settings: settings);
+    }
+  }
+
+  void _publish(
+      {required PubSpec pubspec,
+      required String url,
+      required String pathToProject,
+      required OnePubSettings settings}) {
     pubspec.publishTo.set(url);
-    pubspec.saveTo(join(pathToProject, 'pubspec.yaml'));
+    pubspec.saveTo(p.join(pathToProject, 'pubspec.yaml'));
 
     print('''
-${pubspec.name.value} has been marked as a private package for the organisation ${settings.organisationName}.
+${blue('${pubspec.name.value} has been marked as a private package for the organisation ${settings.organisationName}')}.
 
 Run 'dart/flutter pub publish' to publish ${pubspec.name.value} to OnePub
 
 See ${settings.guidePublishOnePubUrl}
 ''');
+  }
+
+  Future<bool> _checkForNone(
+      {required PubSpec pubspec,
+      required String currentOrganisationName}) async {
+    /// If it is set to none confirm that they want to publish the package.
+    if (pubspec.publishTo.isNone()) {
+      print(red(
+          '${pubspec.name.value} is currently marked as NOT for publishing.\n'));
+      if (!await confirm('Do you still wish to continue?')) {
+        print(red('Action cancelled'));
+        return false;
+      }
+      print('\n');
+    }
+    return true;
+  }
+
+  Future<bool> _checkForSelf(
+      {required PubSpec pubspec,
+      required String currentOrganisationName,
+      required String newOrgUrl}) async {
+    if (!pubspec.publishTo.missing && !pubspec.publishTo.isNone()) {
+      // publish_to is set to a url
+      if (pubspec.publishTo.value == newOrgUrl) {
+        print(orange(
+            '${pubspec.name.value} is already a private package of $currentOrganisationName.'));
+        return false;
+      }
+    }
+    return true;
+  }
+
+  Future<bool> _checkForOther(
+      {required PubSpec pubspec,
+      required String currentOrganisationName,
+      required String newOrgUrl}) async {
+    if (!pubspec.publishTo.missing && !pubspec.publishTo.isPubDev() && !pubspec.publishTo.isNone()) {
+      // publish_to is set to a url
+      if (pubspec.publishTo.value != newOrgUrl) {
+        print(orange('${pubspec.name.value} is already a private package '
+            'for another organisation!\n'));
+        if (!await confirm('Do you want to change the organisation to '
+            '$currentOrganisationName?')) {
+          print(red('Action cancelled'));
+          return false;
+        }
+      }
+      return true;
+    }
+    return true;
   }
 
   /// Working directory defaults to the pwd but a unit test
