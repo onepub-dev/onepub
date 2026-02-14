5c189d87cbbb6c7e2220c2566fec363afc6b627d
Brett Sutton <bsutton@noojee.com.au>
2026-02-14 14:46:14 +1100

    modified our version of printerr so that it will be captured by calls to the dcli capture method. This is a hack as we have had to copy constants from dcli.

--- lib/src/util/printerr.dart
+++ lib/src/util/printerr.dart
@@ -1,4 +1,18 @@
+import 'dart:async';
 import 'dart:io';
+
+/// This is a bit of a hack as we have copied these from dcli_core to avoid
+/// a dependency on the whole package. We only need
+/// callback used when overloading [printerr] in a DCliZone.
+typedef CaptureZonePrintErr = void Function(String);
+
+/// This class is highly experimental - use at your own risk.
+/// It is designed to capture any output to print or printerr
+/// within the scope of the callback.
+/// Key to the overloading [printerr] function.
+
+/// Key to the overloading [printerr] function.
+const capturePrinterrKey = 'printerr';
 
 /// [printerr] provides the equivalent functionality to the
 /// standard Dart print function but instead writes
@@ -8,6 +22,14 @@ import 'dart:io';
 /// out to stderr and expected output to stdout.
 ///
 /// [line] the line to write to stderr.
-void printerr(String? line) {
-  stderr.writeln(line);
+void printerr(Object? object) {
+  final line = '$object';
+
+  /// Co-operate with runDCliZone
+  final overloaded = Zone.current[capturePrinterrKey] as CaptureZonePrintErr?;
+  if (overloaded != null) {
+    overloaded(line);
+  } else {
+    stderr.writeln(line);
+  }
 }
